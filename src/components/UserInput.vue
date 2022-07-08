847fdd1804c53a1884f20579fbf5e1d56b2c658e
Abdulaziz Alsafadi <iweb1700@hotmail.com>
2022-07-08 21:43:57 +0300

    emp placeholder name

--- src/components/UserInput.vue
+++ src/components/UserInput.vue
@@ -2,7 +2,7 @@
 <h3>اسم الموظف</h3>
 <input  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" :value="formData.empName"
 @input="ChangeFormData({empName:$event.target.value})"
-placeholder="مثال:سارا خالد الحربي"
+placeholder="مثال: سارة عبدالله محمد"
 />
 </template>
 <script>
